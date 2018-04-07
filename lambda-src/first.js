80290a12590594d8e3e8579a42b36d32c15073e3
sakapun <sakapun5th@gmail.com>
2018-04-07 17:03:12 +0900

    deploy

--- lambda-src/first.js
+++ lambda-src/first.js
@@ -1,6 +1,20 @@
-exports.handler = function(event, context, callback) {
+import * as line from "@line/bot-sdk";
+
+const config = {
+    channelAccessToken: process.env.TOKEN,
+    channelSecret: process.env.SECRET_KEY
+};
+
+const client = new line.Client(config);
+
+exports.handler = async function(event, context, callback) {
+    client.pushMessage("1572918706", {
+        type: "text",
+        text: "hello world!"
+    });
+
     callback(null, {
         statusCode: 200,
-        body: "Hello, World"
+        body: JSON.stringify(client, null, 2)
     });
 }
