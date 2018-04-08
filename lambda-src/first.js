ef5661547977b752e367eddbcb55e92dc637442e
sakapun <sakapun5th@gmail.com>
2018-04-08 18:03:12 +0900

    S3へのファイルアップロードができた

--- lambda-src/first.js
+++ lambda-src/first.js
@@ -1,5 +1,7 @@
 import * as line from "@line/bot-sdk";
 
+import {uploadS3} from "../utils/image_uploader";
+
 const config = {
     channelAccessToken: process.env.TOKEN,
     channelSecret: process.env.SECRET_KEY
@@ -11,38 +13,37 @@ exports.handler = function(event, context, callback) {
     const events = JSON.parse(event.body).events;
      events.map(async (ev) => {
          console.log(ev);
-         // const afdasidfa = await client.getMessageContent(ev.message.id);
-         // // console.log(afdasidfa);
-         // afdasidfa.on('data', (data) => {
-         //     console.log(data);
-         // })
+         const afdasidfa = await client.getMessageContent(ev.message.id);
+         // console.log(afdasidfa);
+         const pugera = (...ev) => {console.log(ev)};
+         uploadS3(afdasidfa, "hogehoge"+ new Date().getTime() + ".jpg", pugera);
 
-         client.replyMessage(ev.replyToken, {
-             "type": "template",
-             "altText": "this is a confirm template",
-             "template": {
-                 "type": "confirm",
-                 "text": "Are you sure?",
-                 "actions": [
-                     {
-                         "type": "message",
-                         "label": "Yes",
-                         "text": "mu"
-                     },
-                     {
-                         "type": "message",
-                         "label": "No",
-                         "text": "no"
-                     }
-                 ]
-             }
-         });
+         // client.replyMessage(ev.replyToken, {
+         //     "type": "template",
+         //     "altText": "this is a confirm template",
+         //     "template": {
+         //         "type": "confirm",
+         //         "text": "Are you sure?",
+         //         "actions": [
+         //             {
+         //                 "type": "message",
+         //                 "label": "Yes",
+         //                 "text": "mu"
+         //             },
+         //             {
+         //                 "type": "message",
+         //                 "label": "No",
+         //                 "text": "no"
+         //             }
+         //         ]
+         //     }
+         // });
      });
 
 
 
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify("hello world", null, 2)
+        body: JSON.stringify(uploadS3, null, 2)
     });
 }
--- utils/image_uploader.js
+++ utils/image_uploader.js
@@ -1,17 +1,41 @@
-const fs = require("fs");
-const request = require("request");
 require("dotenv").config({path: "../.env"});
 
-console.log(process.env.HOGE);
+const aws = require("aws-sdk");
+// var inspect = require('util').inspect;
 
-const imageStream = fs.createReadStream("icon_03.png");
+// console.log(aws.config);
+const s3 = new aws.S3();
 
-const unsplashit = request("https://source.unsplash.com/1600x900/?nature,water");
+// Define s3-upload-stream with S3 credentials.
+const s3Stream = require('s3-upload-stream')(s3);
 
-imageStream.on("end", () => {
-    console.log("unsplashit")
-})
+// Handle uploading file to Amazon S3.
+// Uses the multipart file upload API.
+function uploadS3 (readStream, key, callback) {
+    var upload = s3Stream.upload({
+        Bucket: 'aws-nodejs-ecma-script-d-serverlessdeploymentbuck-18q97jmjxx8rb',
+        Key: key,
+        ACL: "public-read",
+        ContentType: "image/png"
+    });
 
-const hoge = fs.createWriteStream("test.png");
+    // Handle errors.
+    upload.on('error', function (err) {
+        callback(err);
+    });
 
-unsplashit.pipe(hoge);
+    // Handle progress.
+    upload.on('part', function (details) {
+        // console.log(inspect(details));
+    });
+
+    // Handle upload completion.
+    upload.on('uploaded', function (details) {
+        callback(details);
+    });
+
+    // Pipe the Readable stream to the s3-upload-stream module.
+    readStream.pipe(upload);
+}
+
+module.exports.uploadS3 = uploadS3;
